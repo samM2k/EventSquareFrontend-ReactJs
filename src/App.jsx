1add99520f3d06b31d64e8fa324a817d65430c4d
samM2k <sam.marshall2000@hotmail.com>
2023-10-17 16:45:19 +1100

    refactor component

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginModel from './Models/LoginModel.js'
-
+import LoggedInView from './Views/LoggedInView'
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [loginModel, setLoginModel] = useState(null);
+    const loginModel = new LoginModel();
     const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        if (!loginModel) {
-            const newLoginModel = new LoginModel();
-            setLoginModel(newLoginModel);
-        }
-    }, [])
+    const handleLogin = () => {
+        loginModel.Login("user@example.com", "Test123!")
+            .then(result => setUserIsLoggedIn(result));
+    }
 
-    if (userIsLoggedIn)
+    if (userIsLoggedIn) {
         return (
-            <>
-                <div>
-                    <a href="https://vitejs.dev" target="_blank">
-                        <img src={viteLogo} className="logo" alt="Vite logo" />
-                    </a>
-                    <a href="https://react.dev" target="_blank">
-                        <img src={reactLogo} className="logo react" alt="React logo" />
-                    </a>
-                </div>
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={()=>setCount(count+1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.jsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
-            </>
+                <LoggedInView />
         );
-    else 
+    }
+    else {
+
         return <>
-            <button onClick={function() {
-                loginModel.Login("user@example.com", "Test123!").then((result) => setUserIsLoggedIn(result))
-            }}>Login</button>
+            <button onClick={handleLogin}>Login</button>
         </>
+    } 
 }
 
 export default App
