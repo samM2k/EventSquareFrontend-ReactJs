376b491d20406ca5a8fa9420bd05d811c657028d
samM2k <sam.marshall2000@hotmail.com>
2023-10-17 20:21:29 +1100

    move loginmodel contruction to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import LoginModel from './Models/LoginModel.js'
+
+
+const loginModel = new LoginModel();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <App loginModel={loginModel} />
   </React.StrictMode>,
 )
