8540143739ef51ead823a44cbc12c81caed075d9
samM2k <sam.marshall2000@hotmail.com>
2023-10-22 16:46:34 +1100

    add state to fields on new event view

--- src/Views/NewEventView.jsx
+++ src/Views/NewEventView.jsx
@@ -5,6 +5,16 @@ import OverlayButton from '../Components/OverlayButton';
 
 function NewEventView({ isAuthorized }) {
     const [locationInput, setLocationInput] = useState(null);
+    const [nameInput, setNameInput] = useState("");
+    const [descriptionInput, setDescriptionInput] = useState("");
+    const [startDateInput, setStartDateInput] = useState("");
+    const [startTimeInput, setStartTimeInput] = useState("");
+    const [endDateInput, setEndDateInput] = useState("");
+    const [endTimeInput, setEndTimeInput] = useState("");
+    const [isPhysicalInput, setIsPhysicalInput] = useState(false);
+    const [isVirtualInput, setIsVirtualInput] = useState(false);
+    const [isPublicInput, setIsPublicInput] = useState(false);
+
 
 
     return (<div className="new-event-container">
@@ -12,57 +22,54 @@ function NewEventView({ isAuthorized }) {
             <div className='display-flex-column form-column'>
                 <div className='field'>
                     <label>Name</label>
-                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Name" defaultValue={nameInput} onChange={setNameInput} />
                 </div>
 
                 <div className='field'>
                     <label>Description</label>
-                    <textarea placeholder="Description" />
+                    <textarea placeholder="Description" defaultValue={descriptionInput} onChange={setDescriptionInput} />
                 </div>
 
                 <div className='field'>
                     <label>Location</label>
-                    <LocationPicker InitialValue={locationInput} LocationChangedCallback={(loc) => {
-                        console.log(loc);
-                        setLocationInput(loc)
-                    }} />
+                    <LocationPicker InitialValue={locationInput} LocationChangedCallback={setLocationInput} />
                 </div>
             </div>
             <div className='display-flex-column form-column'>
                 <div className='field'>
                     <label>Start Date</label>
-                    <input type="date" placeholder="Start date" />
+                    <input type="date" placeholder="Start date" defaultValue={startDateInput} onChange={setStartDateInput} />
                 </div>
 
                 <div className='field'>
                     <label>Start Time</label>
-                    <input type="time" placeholder="Start date" />
+                    <input type="time" placeholder="Start Time" defaultValue={startTimeInput} onChange={setStartTimeInput} />
                 </div>
 
                 <div className='field'>
                     <label>End Date</label>
-                    <input type="date" placeholder="Start date" />
+                    <input type="date" placeholder="End date" defaultValue={endDateInput} onChange={setEndDateInput} />
                 </div>
 
                 <div className='field'>
                     <label>End Time</label>
-                    <input type="time" placeholder="Start date" />
+                    <input type="time" placeholder="End time" defaultValue={endTimeInput} onChange={setEndTimeInput} />
                 </div>
 
             </div>
             <div className='display-flex-column form-column centered-vertically'>
                 <div className='display-flex-row field'>
-                    <input type="checkbox" />
+                    <input type="checkbox" defaultChecked={isPhysicalInput} onChange={setIsPhysicalInput} />
                     <label>Physical</label>
                 </div>
 
                 <div className='display-flex-row field'>
-                    <input type="checkbox" />
+                    <input type="checkbox" defaultChecked={isVirtualInput} onChange={setIsVirtualInput} />
                     <label>Virtual</label>
                 </div>
 
                 <div className='display-flex-row field'>
-                    <input type="checkbox" />
+                    <input type="checkbox" defaultChecked={isPublicInput} onChange={setIsPublicInput} />
                     <label>Public</label>
                 </div>
 
