1b65763a8bee1dfac67aa20839d5a09224b9e2b0
samM2k <sam.marshall2000@hotmail.com>
2023-10-24 19:45:37 +1100

    add back arrow to navbar

--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar({ items }) {
+    const navigate = useNavigate();
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button className={location.pathname == "/" ? 'navbar-toggler hidden' : 'navbar-toggler'} onClick={() => navigate(-1)}><ion-icon name="chevron-back" id="back-icon" /></button>
             <Link className="navbar-brand" to="/">EventSquare</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
