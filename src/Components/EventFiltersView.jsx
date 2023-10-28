416f8c991e9b40b8f240aab4f02e2929d6296aca
samM2k <sam.marshall2000@hotmail.com>
2023-10-28 14:01:04 +1100

    styling event filters view

--- src/Components/EventFiltersView.jsx
+++ src/Components/EventFiltersView.jsx
@@ -1,33 +1,38 @@
+import './EventFiltersView.css';
+
 function EventFiltersView({ nameFilter, setNameFilter, descriptionFilter, setDescriptionFilter, publicFilter, setPublicFilter, applyFiltersCallback }) {
     return (
-        <div className="filters">
+        <div className="filters-view">
+            <div className='filters'>
+                {/* Name filter */}
+                <div className="filter">
+                    <label htmlFor="name-filter-input">
+                        Name
+                    </label>
+                    <input id="name-filter-input" value={nameFilter} onChange={(e) => {
+                        setNameFilter(e.target.value)
+                    }} type="text" />
+                </div>
 
-            {/* Name filter */}
-            <label htmlFor="name-filter-input">
-                Name
-            </label>
-            <input id="name-filter-input" value={nameFilter} onChange={(e) => {
-                setNameFilter(e.target.value)
-            }} type="text" />
-
-            {/* Descriptoin filter */}
-            <label htmlFor="desc-filter-input">
-                Description
-            </label>
-            <input id="desc-filter-input" value={descriptionFilter} onChange={(e) => {
-                setDescriptionFilter(e.target.value)
-            }} type="text" />
-
-            {/* Public filter */}
-            <div className="checkbox-filter">
-                <input id="public-filter-checkbox" value={publicFilter} onChange={(e) => {
-                    setPublicFilter(e.target.checked)
-                }} type="checkbox" />
-                <label htmlFor="public-filter-checkbox">
-                    Public
-                </label>
+                {/* Descriptoin filter */}
+                <div className="filter">
+                    <label htmlFor="desc-filter-input">
+                        Description
+                    </label>
+                    <input id="desc-filter-input" value={descriptionFilter} onChange={(e) => {
+                        setDescriptionFilter(e.target.value)
+                    }} type="text" />
+                </div>
+                {/* Public filter */}
+                <div className="filter checkbox-filter">
+                    <input id="public-filter-checkbox" value={publicFilter} onChange={(e) => {
+                        setPublicFilter(e.target.checked)
+                    }} type="checkbox" />
+                    <label htmlFor="public-filter-checkbox">
+                        Public
+                    </label>
+                </div>
             </div>
-
             <button onClick={applyFiltersCallback}>Apply</button>
         </div>
     );
