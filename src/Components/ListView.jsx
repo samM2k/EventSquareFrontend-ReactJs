678801a63a09168f88d34e13d5a4b723e2bc2d29
samM2k <sam.marshall2000@hotmail.com>
2023-10-28 14:20:17 +1100

    groundwork for pagination

--- src/Components/ListView.jsx
+++ src/Components/ListView.jsx
@@ -1,9 +1,17 @@
 import OverlayButton from "./OverlayButton.jsx"
 import { useNavigate } from "react-router-dom";
 import './ListView.css';
+import React, { useEffect, useState } from "react";
 
 function ListView({ IsAuthorized, children, AddEntryRoute }) {
     const navigate = useNavigate();
+    const [childrenToShow, setChildrenToShow] = useState(null)
+    const [page, setPage] = useState(0);
+
+
+    useEffect(() => {
+        setChildrenToShow(React.Children.toArray(children))
+    }, [children])
 
     return (
         <div className='list-view'>
@@ -12,9 +20,19 @@ function ListView({ IsAuthorized, children, AddEntryRoute }) {
                     <OverlayButton ioniconName="add" onClick={() => navigate(AddEntryRoute)} />
                     : null
             }
-            {children}
+            {childrenToShow}
             <div className="events-list-pagination">
                 This is where the pagination will be.
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href="#" onClick={() => setPage((page - 1) > 0 ? page - 1 : 0)}>Previous</a></li>
+                        <li className="page-item active"><a className="page-link" href="#" onClick={() => setPage(0)}>1</a></li>
+                        <li className="page-item"><a className="page-link" href="#" onClick={() => setPage(1)}>2</a></li>
+                        <li className="page-item"><a className="page-link" href="#" onClick={() => setPage(2)}>3</a></li>
+                        <li className="page-item"><a className="page-link" href="#" onClick={() => setPage(page + 1)}>Next</a></li>
+                    </ul>
+                </nav>
+                Current Page: {page}
             </div>
         </div>
     );
